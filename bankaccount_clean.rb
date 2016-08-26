ba24392494564ea163fdfae17c2781969bccf57f
Trish Griego <pmgriego@gmail.com>
2016-08-26 15:57:40 -0700

    Holy cow, after hours of moving methods and classes around. I think I may have a working MVP. Saving before building final piece. The reset checks method.Dun Dun dun..

--- bankaccount_clean.rb
+++ bankaccount_clean.rb
@@ -8,8 +8,7 @@ module BankAccount
 
 
   class Account
-    WITHDARAWL_FEE = 0
-    ACCOUNT_MIN_BALANCE = 0
+
     attr_accessor :id, :balance, :open_date
     # could I make this a hash? So, that I don't have to build it below?
 
@@ -17,10 +16,12 @@ module BankAccount
       @id = account_info[:id]
       @balance = account_info[:balance]
       @open_date = account_info[:open_date]
+      @withdraw_fee = 0
+      @account_min_balance = 0
 
-      if @balance < self.class::ACCOUNT_MIN_BALANCE
+      if @balance < @account_min_balance
         # why should I initialize argument error?
-        raise ArgumentError.new, "cant create account with negative balance"
+        raise ArgumentError, "cant create account with negative balance"
       end
     end
 
@@ -29,15 +30,15 @@ module BankAccount
     #   catch_error false
     # end
     def to_s
-      return "Id: #{id}, Balance: #{balance}, Open Date: #{open_date}"
+      return "Account ID: #{id}, Balance: #{balance}, Open Date: #{open_date}".colorize(:blue)
     end
 
     def withdraw(amount)
-      if (@balance - (amount + self.class::WITHDARAWL_FEE)) >=
-        self.class::ACCOUNT_MIN_BALANCE
-          @balance -= (amount + self.class::WITHDARAWL_FEE)
+      if (@balance - (amount + self.class::withdraw_fee)) >=
+        self.class::account_min_balance
+          @balance -= (amount + self.class::withdraw_fee)
           return @balance
-      elsif (@balance - amount) < self.class::ACCOUNT_MIN_BALANCE
+      elsif (@balance - amount) < self.class::account_min_balance
         puts "That's impossible you cannot create account with insuficient funds."
         return @balance
       end
@@ -69,7 +70,7 @@ module BankAccount
 
     def self.find(id)
       accounts_search = []
-      accounts_search = Bank::Account.all("./support/accounts.csv")
+      accounts_search = BankAccount::Account.all("./support/accounts.csv")
 
       accounts_search.each do |account|
         if account.id == id.to_s
@@ -78,13 +79,111 @@ module BankAccount
       end
     end
   end
-end
+
 
 #
 ## :id, ##:balance, ##:open_date
 account1 = BankAccount::Account.new({:id => 2200, :balance => 30000, :open_date => Time.parse('2016-01-17 12:12:12 -0800')})
 puts account1
 # output: Id: 2200, Balance: 30000, Open Date: 2016-01-17 12:12:12 -0800
+
+
+  class SavingsAccount < Account
+# Input rate is assumed to be a percentage (i.e. 0.25).
+# The formula for calculating interest is balance * rate/100
+  # starting of my counter at zero
+  @checks_used_in_month = 0
+
+  def initialize(account_info)
+      super
+      @withdraw_fee = 2
+      @account_min_balance = 10
+# The initial balance cannot be less than $10.
+      if @account_min_balance < 10
+#If it is, this will raise an ArgumentError
+        raise ArgumentError, "your account balance is < $10"
+      else
+        return "Balance: #{@balance}"
+      end
+  end
+# Updated withdrawal functionality:
+# Each withdrawal 'transaction' incurs a fee of $2 that is taken out of the balance.
+    def withdraw_with_check(amount)
+      if(@balance - amount) > -1000 && @checks_used_in_month < 3
+        @balance -=amount
+        return @balance
+      end
+    end
+#add_interest(rate): Calculate the interest on the balance and add the interest to the balance. Return the interest that was calculated and added to the balance (not the updated balance).
+    def add_interest(rate=0.25)
+      interest = @balance * rate / 100.0
+      @balance += interest
+      return interest
+    end
+  end
+
+
+# account1 = BankAccount::SavingsAccount.new({:id => 2200, :balance => 30000, :open_date => Time.parse('2016-01-17 12:12:12 -0800')})
+# puts account1.add_interest()
+
+# Create a CheckingAccount class which should inherit behavior from the Account class.
+  class CheckingAccount < Account
+  # Create a CheckingAccount class which should inherit behavior from the Account
+      @withdraw_fee = 100
+      @check_fee = 200
+
+      def initialize(account_info)
+        super
+        @checks_used_in_month = 0
+      end
+# Updated withdrawal functionality:
+      def withdraw_with_check(amount)
+        if(@balance - amount) > -1000 && @checks_used_in_month < 3
+          @balance -=amount
+# Each withdrawal 'transaction' incurs a fee of $1 that is taken out of the balance.
+          @checks_used_in_month += 1
+#  Returns the updated account balance.
+          return @balance
+        elsif (@balance - amount) > -1000 && @checks_used_in_month >=3
+          @balance -= (amount + @@check_fee)
+          @checks_used_in_month +=1
+          return @balance
+        else
+          puts "You don't have any $$ to write any checks, fool! Current Balance: #{@balance}"
+        end
+      end
+  end
+end
+
+
+account1 = BankAccount::CheckingAccount.new({:id => 2200, :balance => 30000, :open_date => Time.parse('2016-01-17 12:12:12 -0800')})
+puts account1.withdraw_with_check(200)
+
+
+
+=begin
+    Create a CheckingAccount class which should inherit behavior from the Account class. It should include the following updated functionality:
+
+    Updated withdrawal functionality:
+    Each withdrawal 'transaction' incurs a fee of $1 that is taken out of the balance. Returns the updated account balance.
+
+    Does not allow the account to go negative. Will output a warning message and return the original un-modified balance.
+
+    #withdraw_using_check(amount): The input amount gets taken out of the account as a result of a check withdrawal. Returns the updated account balance.
+    Allows the account to go into overdraft up to -$10 but not any lower
+    The user is allowed three free check uses in one month, but any subsequent use adds a $2 transaction fee
+    #reset_checks: Resets the number of checks used to zero
+=end
+#
+# def reset_checks
+#     @checks_used_in_month = 0
+# end
+
+
+
+
+
+
 
 
 ##########################
