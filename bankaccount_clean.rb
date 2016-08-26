22620dca47b1888f6cc1db7f7c55d23a2a0d5fa7
Trish Griego <pmgriego@gmail.com>
2016-08-26 11:17:26 -0700

    Finally refactored parent class Account. It works as intended at the moment. I haven't begun adding in savings account class. Hopefully, it will be easier to build.

--- bankaccount_clean.rb
+++ bankaccount_clean.rb
@@ -1,29 +1,35 @@
 require 'csv'
 require 'colorize'
 require 'awesome_print'
-
+require 'time'
 # rename item Bank?
 module BankAccount
   # credit idea of setting constnats from Rowan.
-  WITHDARAWL_FEE = 0
-  ACCOUNT_MIN_BALANCE = 0
+
 
   class Account
+    WITHDARAWL_FEE = 0
+    ACCOUNT_MIN_BALANCE = 0
     attr_accessor :id, :balance, :open_date
     # could I make this a hash? So, that I don't have to build it below?
+
     def initialize(account_info)
       @id = account_info[:id]
       @balance = account_info[:balance]
       @open_date = account_info[:open_date]
 
       if @balance < self.class::ACCOUNT_MIN_BALANCE
-        raise ArgumentError, "cant create account with negative balance"
+        # why should I initialize argument error?
+        raise ArgumentError.new, "cant create account with negative balance"
       end
     end
 
-    def catch_error(amount)
-      raise TypeError, "You lack sufficient funds" if amount == false
-      catch_error false
+    # def catch_error(amount)
+    #   raise TypeError, "You lack sufficient funds" if amount == false
+    #   catch_error false
+    # end
+    def to_s
+      return "Id: #{id}, Balance: #{balance}, Open Date: #{open_date}"
     end
 
     def withdraw(amount)
@@ -32,14 +38,14 @@ module BankAccount
           @balance -= (amount + self.class::WITHDARAWL_FEE)
           return @balance
       elsif (@balance - amount) < self.class::ACCOUNT_MIN_BALANCE
-        puts "You have insuficient funds"
+        puts "That's impossible you cannot create account with insuficient funds."
         return @balance
       end
     end
 
     def deposit(amount)
       @balance = @balance + amount
-      puts "#{name} Deposited: $#{amount}. New Balance: $#{@balance}."
+      # puts "#{name} Deposited: $#{amount}. New Balance: $#{@balance}."
       return @balance
     end
 
@@ -53,7 +59,7 @@ module BankAccount
 # changed this to be more explicit about holding my data.
       CSV.foreach(grab_csv) do |row|
         id = row[0]
-        balance = row[1] # convert to interger
+        balance = row[1].to_i # convert to interger
         open_date = row[2]
         account_inception = {id: id, balance: balance, open_date: open_date}
         account_list << self.new(account_inception)
@@ -66,7 +72,7 @@ module BankAccount
       accounts_search = Bank::Account.all("./support/accounts.csv")
 
       accounts_search.each do |account|
-        if account.id == id
+        if account.id == id.to_s
           return account
         end
       end
@@ -74,10 +80,12 @@ module BankAccount
   end
 end
 
+#
+## :id, ##:balance, ##:open_date
+account1 = BankAccount::Account.new({:id => 2200, :balance => 30000, :open_date => Time.parse('2016-01-17 12:12:12 -0800')})
+puts account1
+# output: Id: 2200, Balance: 30000, Open Date: 2016-01-17 12:12:12 -0800
 
-
-#module ends
-# end
 
 ##########################
 #         WAVE 3         #
